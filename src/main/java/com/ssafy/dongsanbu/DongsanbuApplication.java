3ca36119036f41c94d775e0ef3a6cd2dcd2b7649
minnim1010 <pms1010123@daum.net>
2024-05-10 12:19:47 +0900

    Initial commit

---
+++ src/main/java/com/ssafy/dongsanbu/DongsanbuApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dongsanbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongsanbuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongsanbuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/dongsanbu/DongsanbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dongsanbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongsanbuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
