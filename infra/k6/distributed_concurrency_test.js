2327335e35c897ac3ee9c71de59c379a740517ce
minnim1010 <pms1010123@daum.net>
2024-05-23 13:36:57 +0900

    fix: 카프카 producer ip 수정

--- infra/k6/distributed_concurrency_test.js
+++ infra/k6/distributed_concurrency_test.js
@@ -1,10 +1,10 @@
 // GET
 import http from "k6/http";
-import {check} from "k6";
+import {check, sleep} from "k6";
 
 export const options = {
-    vus: 30, // 가상 사용자 수
-    duration: "30s", // 테스트 시간
+    vus: 5, // 가상 사용자 수
+    duration: "10s", // 테스트 시간
 
     thresholds: {
         http_req_duration: ['p(95)<100']    // 95%가 100ms 안에 응답을 받아야 함
@@ -15,24 +15,26 @@ export default function () {
     let num = getRandomValue(1, 2);
     if (num === 1) {
         transferApi1();
-    } else transferApi1();
+    } else transferApi2();
+
+    sleep(1)
 }
 
 function getRandomValue(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const base_url1 = "http://34.64.125.6:8080";
+const base_url1 = "http://34.47.68.121:8080";
 const base_url2 = "http://13.124.221.0:8080";
 
 function transferApi1() {
-    const fromAccount = "mj82227053";
+    const fromAccount = "mj00";
     const toAccount = "jh-USER0ACCOUNT0"
     const url = base_url1 + '/api/v1/account/transfer';
     const payload = JSON.stringify({
         fromAccountNumber: fromAccount,
         toAccountNumber: toAccount,
-        amount: 1500,
+        amount: 500,
     });
     const params = {
         headers: {
@@ -53,12 +55,12 @@ function transferApi1() {
 
 function transferApi2() {
     const fromAccount = "jh-USER0ACCOUNT0";
-    const toAccount = "mj82227053"
+    const toAccount = "mj00"
     const url = base_url2 + '/api/v1/account/transfer';
     const payload = JSON.stringify({
         fromAccountNumber: fromAccount,
         toAccountNumber: toAccount,
-        amount: 1500,
+        amount: 500,
     });
     const params = {
         headers: {
